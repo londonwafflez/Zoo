5693f6c59b5661491b3c695d516c4e0a31861a21
londonwafflez <93176330+londonwafflez@users.noreply.github.com>
2021-11-06 17:42:55 -0700

    Update renameThisLater.java

--- renameThisLater.java
+++ renameThisLater.java
@@ -1,5 +1,31 @@
-public class SOMETHING {
+import java.util.ArrayList;
 
+public class Monkey {
+
+  String name;
+  int yearsOld;
+  char gender;
+  double height;
+  
+  ArrayList<> generalInfoList = new ArrayList;
+  ArrayList<String> monkeyList = new ArrayList<String>();
+  int[] monkeyAges = new int[5];
+  double[] monkeyHeights = new double[5];
+  
+  public Monkey(int age; String monkeyName; char monkeyGender; double monkeyHeight) {
+    monkeyName = name;
+    
+    monkeyList.add()
+    System.out.println(name + ", the monkey has been registered.");
+  }
+  
+  
+  
   public static main(String [] args) {
+  
+  }
+  
+  public String toString() {
+  return this.name + 
   }
 } 
