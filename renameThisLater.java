935a58297051cbb9d757869b7537f0d65ae335e2
londonwafflez <93176330+londonwafflez@users.noreply.github.com>
2021-11-08 16:55:55 -0800

    Update renameThisLater.java

--- renameThisLater.java
+++ renameThisLater.java
@@ -3,29 +3,38 @@ import java.util.ArrayList;
 public class Monkey {
 
   String name;
-  int yearsOld;
-  char gender;
+  int age;
+  String gender;
   double height;
   
-  ArrayList<> generalInfoList = new ArrayList;
-  ArrayList<String> monkeyList = new ArrayList<String>();
+  ArrayList generalInfoList = new ArrayList();
+  public ArrayList<String> monkeyList = new ArrayList<String>();
   int[] monkeyAges = new int[5];
   double[] monkeyHeights = new double[5];
   
-  public Monkey(int age, String monkeyName, char monkeyGender, double monkeyHeight) {
-    monkeyName = name;
-    
-    monkeyList.add()
+  public Monkey(int monkeyAge, String monkeyName, char monkeyGender, double monkeyHeight) {
+    age = monkeyAge;
+    name = monkeyName;
+    height = monkeyHeight;
+    if (monkeyGender.equals('m') || monkeyGender.equals('M')) {
+        gender = "male";
+    } else if (monkeyGender.equals('f') || monkeyGender.equals('F')) {
+        gender = "female";
+    } else {
+        gender = "other";
+    }
+    }
+    monkeyList.add(name);
     System.out.println(name + ", the monkey has been registered.");
   }
   
   
   
-  public static main(String [] args) {
-  
+  public static void main(String[] args) {
+  System.out.println("Running");
   }
   
   public String toString() {
-  return this.name + 
+  return name + ", the " + gender + " monkey is " + age + " years old";
   }
 } 
