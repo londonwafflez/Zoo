db63a67076a0f3efaa71dc033cae2cae45bd5972
londonwafflez <93176330+londonwafflez@users.noreply.github.com>
2021-11-08 20:04:14 -0800

    Update renameThisLater.java

--- renameThisLater.java
+++ renameThisLater.java
@@ -1,24 +1,53 @@
-public class Dog {
-  String breed;
-  boolean hasOwner;
+//Importing ArrayList
+import java.util.ArrayList;
+
+public class Monkey {
+
+  //Defining variables for later
+  String name;
   int age;
+  String gender;
+  double height;
   
-  public Dog(String dogBreed, boolean dogOwned, int dogYears) {
-    System.out.println("Constructor invoked!");
-    breed = dogBreed;
-    hasOwner = dogOwned;
-    age = dogYears;
+  //Defining lists
+  ArrayList generalInfoList = new ArrayList();
+  public ArrayList<String> monkeyList = new ArrayList<String>();
+  int[] monkeyAges = new int[5];
+  double[] monkeyHeights = new double[5];
+  
+  //constructor for creating a Monkey
+  public Monkey(String monkeyName, int monkeyAge, char monkeyGender, double monkeyHeight) {
+    //Sync added values to more public ones
+    age = monkeyAge;
+    name = monkeyName;
+    height = monkeyHeight;
+
+    //Taking char and making it a string for gender
+    if (monkeyGender == 'm' || monkeyGender == 'M') {
+        gender = "male";
+    } else if (monkeyGender == 'f' || monkeyGender == 'F') {
+        gender = "female";
+    } else {
+        gender = "other";
+    }
+    //Add Monkey to MonkeyList and finish out the constructor
+    monkeyList.add(name);
+    System.out.println(name + ", the monkey has been registered.");
   }
+
+  //add method here
   
   public static void main(String[] args) {
-    System.out.println("Main method started");
-    Dog fido = new Dog("poodle", false, 4);
-    Dog nunzio = new Dog("shiba inu", true, 12);
-    boolean isFidoOlder = fido.age > nunzio.age;
-    int totalDogYears = nunzio.age + fido.age;
-    System.out.println("Two dogs created: a " + fido.breed + " and a " + nunzio.breed);
-    System.out.println("The statement that fido is an older dog is: " + isFidoOlder);
-    System.out.println("The total age of the dogs is: " + totalDogYears);
-    System.out.println("Main method finished");
+  //Using constructor to define new Monkeys
+  Monkey Henry = new Monkey("Henry", 4, 'm', 11.1);
+  Monkey Jack = new Monkey("Jack", 6, 'm', 13.8);
+  Monkey Jill = new Monkey("Jill", 4, 'f', 10.5);
+  Monkey Emma = new Monkey("Emma", 5, 'f', 12.2);
+  System.out.println(Emma.age);
   }
-}
+  
+  //Adding a toString for when a created Monkey is printed
+  public String toString() {
+  return name + ", the " + gender + " monkey is " + age + " years old";
+  }
+} 
