4950ffa1e13757eb2ade452f646fd7916289e31b
londonwafflez <93176330+londonwafflez@users.noreply.github.com>
2021-12-01 19:19:56 -0800

    Update Monkey.java

--- Monkey.java
+++ Monkey.java
@@ -2,7 +2,7 @@
   import java.util.ArrayList;
   import java.util.Random;
 
-  public class Monkey {
+  public class Monkey extends Animal {
   
     //Defining variables for later
     final String name;
@@ -58,28 +58,7 @@
       }
     }
   
-    public static void main(String[] args) {
-    
 
-    
-      //Using constructor to define new Monkeys
-      Monkey Henry = new Monkey("Henry", 4, 'm', 11.1);
-      Monkey Jack = new Monkey("Jack", 6, 'M', 13.8);
-      Monkey Jill = new Monkey("Jill", 4, 'f', 10.5);
-      Monkey Emma = new Monkey("Emma", 5, 'F', 12.2);
-      Monkey Mary = new Monkey("Mary", 6, 'f', 11.7);
-
-      Henry.addMonkeyToList();
-      Jack.addMonkeyToList();
-      Jill.addMonkeyToList();
-      Emma.addMonkeyToList();
-      Mary.addMonkeyToList();
-
-      //Printing info to make sure everything checks out
-      System.out.println(Henry.name + " is " + Henry.age + " and likes to dance.");
-      System.out.println("What do we know about Emma? \n" + Emma.getMonkeyInfo());
-      System.out.println(monkeyNames.get(2) + ", the third monkey's age is " + monkeyAges[2] + ".");
-    }
     
     //Adding a toString for when a created Monkey is printed
     public String toString() {
