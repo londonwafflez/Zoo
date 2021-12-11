7ec93175f411f81998b1d8c2e7853723993f96f4
londonwafflez <superz006@icloud.com>
2021-12-11 15:00:30 -0800

    changing zoo to main and adding constructor to animal

--- Zoo/Habitat.java
+++ Zoo/Habitat.java
@@ -1,5 +1,24 @@
 package Zoo;
 
 public class Habitat {
+    static int year = 2021;
 
+    //Idea credit goes to Kyle H
+    public static void newLn() {
+        System.out.println("");
+    }
+
+    public static void passTime(int years) {
+
+        year += years;
+        
+        for (int i = 0; i <= years; i++) {
+            for (Monkey monkey : Monkey.monkeyList) {
+                monkey.age += years;
+                monkey.height += Math.random() * 3 - 0 + 1;
+            }
+        }
+
+        System.out.println(years + " years have passed");
+    }
 }
